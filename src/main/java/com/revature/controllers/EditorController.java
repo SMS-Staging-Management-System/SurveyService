53a4537c03e6e4fa51cdbee39ba8f4ff4e2f6c44
Dunieski Otano <46684047+dunieskiotano@users.noreply.github.com>
2019-03-10 03:07:48 -0400

    added comments

--- src/main/java/com/revature/controllers/EditorController.java
+++ src/main/java/com/revature/controllers/EditorController.java
@@ -1,7 +1,10 @@
 package com.revature.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,45 +21,61 @@ import com.revature.models.Editor;
 import com.revature.services.EditorServiceImpl;
 
 @RestController
-@RequestMapping("editors")
+@RequestMapping("editors")//Maps this class to table editors in DB
 public class EditorController {
-
+    
+	//Tells Spring to connect bean automatically
 	@Autowired
 	private EditorServiceImpl eSI;
 
-	@GetMapping("")
+	// we find all editors here using repo method findAll()
+	@GetMapping
 	List<Editor> findAll() {
 		return eSI.findAll();
 	}
 
+	// Obtain id from URL and pass it to method find editor by id
 	@GetMapping("{id}")
 	public Editor findEditorById(@PathVariable int id) {
 		return eSI.findById(id);
 	}
 
+	 /* Obtains email from URL and pass it to method to find editor by email. We used
+	@PostMapping because correct email cant be passed in using
+	GetMapping*/
 	@PostMapping("/email/{email}")
 	public Editor findByEmail(@Valid @RequestBody String email) {
 		return eSI.findByEmail(email);
 	}
 
-	@GetMapping("surveyId/{id}")
-	public List<Editor> findEditorBySurveyId(@PathVariable int id) {
-		return eSI.findBySurveyId(id);
+	@GetMapping("/survey/{id}")
+	public List<Editor> findEditorBySurveyId(@PathVariable int surveyId) {
+		return eSI.findBySurveyId(surveyId);
 	}
-	
+
+	// Creates editor by calling method createEditor from EditorServiceImpl class
 	@PostMapping
 	public Editor createEditor(@RequestBody Editor editor) {
 		return eSI.createEditor(editor);
 	}
-	
+
+	// Updates editor by calling method updateEditor from the EditorServiceImpl
+	// class
 	@PatchMapping
 	public Editor updateEditor(@RequestBody Editor editor) {
 		return eSI.updateEditor(editor);
 	}
+
+    ////Deletes editor by calling method updateEditor from the EditorServiceImpl class
 	@DeleteMapping
-	public void deleteEditor(int id) {
+	public String deleteEditor(int id) {
 		eSI.deleteEditor(id);
+		return "Editor has been successfully deleted";
 	}
-	
+
+//	@ExceptionHandler(Exception.class)
+//	public ResponseEntity<String> handleExceptions(Exception e) {
+//		return new ResponseEntity<String>("An error has occurred", HttpStatus.INTERNAL_SERVER_ERROR);
+//	}
 
 }
